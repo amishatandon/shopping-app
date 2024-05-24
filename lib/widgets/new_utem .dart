b3463678ba0ae89585aae7770618c44a0de6a67b
amishatandon <amishatandon2003@gmail.com>
2024-05-25 00:56:45 +0530

    changes

--- lib/widgets/new_utem .dart
+++ lib/widgets/new_utem .dart
@@ -11,6 +11,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<
+      FormState>(); //gives easy access to underlined widget and keeps the internal state of the widget
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -30,7 +37,13 @@ class _NewItemState extends State<NewItem> {
                 ),
                 validator: (value) {
                   //takes a string value to decide when to validate this form
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return "must be between 1 and 50 characters";
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -41,7 +54,20 @@ class _NewItemState extends State<NewItem> {
                       decoration: const InputDecoration(
                         label: Text('Qunatity'),
                       ),
+                      keyboardType: TextInputType.number,
                       initialValue: '1',
+                      validator: (value) {
+                        //takes a string value to decide when to validate this form
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) ==
+                                null //returns a value to a number
+                            ||
+                            int.tryParse(value)! <= 0) {
+                          return "must be a valid positive number ";
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -65,6 +91,24 @@ class _NewItemState extends State<NewItem> {
                           ),
                         )
                     ], onChanged: (value) {}),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
                   ),
                 ],
               )
